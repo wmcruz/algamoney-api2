5ae9bebcd3883c3ba149c80f9d8c06b018d70516
Wellington Cruz <wellington.max1@gmail.com>
2020-02-23 18:41:30 -0300

    Criação e ajustes de projeção/resumo.

--- src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -5,9 +5,10 @@ import org.springframework.data.domain.Pageable;
 
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.repository.filter.LancamentoFilter;
+import com.example.algamoney.api.repository.projection.ResumoLancamento;
 
 public interface LancamentoRepositoryQuery {
 
 	Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
-
+	Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable);
 }
