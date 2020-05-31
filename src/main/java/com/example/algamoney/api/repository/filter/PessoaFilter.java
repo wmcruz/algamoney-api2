6f44548c971b222e5a4106e568f32ab38d85b3f1
Wellington Max Cruz <wellington.max1@gmail.com>
2020-05-31 18:26:44 -0300

    tImplementação de pesquisa de pessoa, para conter todas informações no Json, conforme Pesquisa de Lançamentos.

---
+++ src/main/java/com/example/algamoney/api/repository/filter/PessoaFilter.java
@@ -0,0 +1,15 @@
+package com.example.algamoney.api.repository.filter;
+
+public class PessoaFilter {
+	// Atributos
+	private String nome;
+
+	// getters an setters
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
