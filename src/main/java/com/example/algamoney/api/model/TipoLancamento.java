9fe2b12f037f41ea7a21f320f112fb624387ac80
Wellington Max Cruz <wellington.max1@gmail.com>
2020-08-19 19:18:59 -0300

    Alteração no Enum para fornecer uma alternativa mais amigável na descrição

--- src/main/java/com/example/algamoney/api/model/TipoLancamento.java
+++ src/main/java/com/example/algamoney/api/model/TipoLancamento.java
@@ -1,6 +1,12 @@
 package com.example.algamoney.api.model;
 
 public enum TipoLancamento {
-	RECEITA,
-	DESPESA
+	RECEITA("Receita"),
+	DESPESA("Despesa");
+
+	private final String descricao;
+
+	TipoLancamento(String descricao) {
+		this.descricao = descricao;
+	}
 }
