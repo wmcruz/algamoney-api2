3ac908221c9f60906387282b265e131374b21076
Wellington Max Cruz <wellington.max1@gmail.com>
2020-08-24 23:37:39 -0300

    Criação de classe e atribut Mail para ser utilizado como propriedade no arquivo de configuração de property do spring.

--- src/main/java/com/example/algamoney/api/config/property/AlgamoneyApiProperty.java
+++ src/main/java/com/example/algamoney/api/config/property/AlgamoneyApiProperty.java
@@ -5,45 +5,92 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 /**
  * Classe reponsavel por configurar propriedades para o sistema
  * Opções uteis para utilizar em ambiente locais e de produção
- * 
+ *
  * @author welyn
  */
 @ConfigurationProperties("algamoney")
 public class AlgamoneyApiProperty {
-	
-	// Atributos
-	private String origemPermitida = "http://localhost:4200";
-	
-	private final Seguranca seguranca = new Seguranca();
-	
-	//Getters and setters
-	public Seguranca getSeguranca() {
-		return seguranca;
-	}
-	
-	public String getOrigemPermitida() {
-		return origemPermitida;
-	}
-	
-	public void setOrigemPermitida(String origemPermitida) {
-		this.origemPermitida = origemPermitida;
-	}
 
-	/**
-	 * Classe responsavel por acoplar opções de segurança
-	 * 
-	 * @author welyn
-	 */
-	public static class Seguranca {
-		
-		private boolean enableHttps;
+    // Atributos
+    private String origemPermitida = "http://localhost:4200";
 
-		public boolean isEnableHttps() {
-			return enableHttps;
-		}
+    private final Seguranca seguranca = new Seguranca();
 
-		public void setEnableHttps(boolean enableHttps) {
-			this.enableHttps = enableHttps;
-		}
-	}
+    private final Mail mail = new Mail();
+
+    //Getters and setters
+    public Seguranca getSeguranca() {
+        return seguranca;
+    }
+
+    public Mail getMail() {
+        return mail;
+    }
+
+    public String getOrigemPermitida() {
+        return origemPermitida;
+    }
+
+    public void setOrigemPermitida(String origemPermitida) {
+        this.origemPermitida = origemPermitida;
+    }
+
+    /**
+     * Classe responsavel por acoplar opções de segurança
+     *
+     * @author welyn
+     */
+    public static class Seguranca {
+
+        private boolean enableHttps;
+
+        public boolean isEnableHttps() {
+            return enableHttps;
+        }
+
+        public void setEnableHttps(boolean enableHttps) {
+            this.enableHttps = enableHttps;
+        }
+    }
+
+    public static class Mail {
+        // Atributos
+        private String host;
+        private Integer port;
+        private String username;
+        private String password;
+
+        // Getters and Setters
+        public String getHost() {
+            return host;
+        }
+
+        public void setHost(String host) {
+            this.host = host;
+        }
+
+        public Integer getPort() {
+            return port;
+        }
+
+        public void setPort(Integer port) {
+            this.port = port;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
 }
