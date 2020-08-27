9782bdeb1407ba44770ab00720c747e36ede61cb
Wellington Max Cruz <wellington.max1@gmail.com>
2020-08-26 23:30:15 -0300

    Criação de um método publico para processamento do template e envio do e-mail.

--- src/main/java/com/example/algamoney/api/mail/Mailer.java
+++ src/main/java/com/example/algamoney/api/mail/Mailer.java
@@ -1,16 +1,19 @@
 package com.example.algamoney.api.mail;
 
+import com.example.algamoney.api.model.Lancamento;
+import com.example.algamoney.api.repository.LancamentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Component
 public class Mailer {
@@ -18,16 +21,39 @@ public class Mailer {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Autowired
+    private TemplateEngine thymeleaf;
+
+    @Autowired
+    private LancamentoRepository repository;
+
     /* Criação de um evento para disparar um e-mail no inicio da aplicação.
      o mesmo método pode ser utilizado sempre que quiséssemos enviar um e-mail */
-    // @EventListener
-    // private void teste(ApplicationReadyEvent event) {
-    //    this.enviarEmail("wellington.max1@gmail.com", Arrays.asList("welyngton_lk@hotmail.com"), "Testando", "Olá!<br />Teste ok.");
-    //    System.out.println("Terminado o envio de e-mail....");
-    //}
+     /*@EventListener
+     private void teste(ApplicationReadyEvent event) {
+         String template = "mail/aviso-lancamentos-vencidos";
 
-    public void enviarEmail(String remetente,
-                            List<String> destinatarios, String assunto, String mensagem) {
+         List<Lancamento> lista = repository.findAll();
+
+         Map<String, Object> variaveis = new HashMap<>();
+         variaveis.put("lancamentos", lista);
+
+         this.enviarEmail("wellington.max1@gmail.com", Arrays.asList("welyngton_lk@hotmail.com"), "Testando", template, variaveis);
+         System.out.println("Terminado o envio de e-mail....");
+    }*/
+
+    public void enviarEmail(String remetente,  List<String> destinatarios, String assunto, String template, Map<String, Object> variaveis) {
+
+        Context context = new Context(new Locale("pt", "BR"));
+
+        variaveis.entrySet().forEach(e -> context.setVariable(e.getKey(), e.getValue()));
+
+        String mensagem = thymeleaf.process(template, context);
+
+        this.enviarEmail(remetente, destinatarios, assunto, mensagem);
+    }
+
+    public void enviarEmail(String remetente, List<String> destinatarios, String assunto, String mensagem) {
         try {
             MimeMessage mimeMessage = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
