ae63e074567bbb5925cc7cda2e2e1d2edcc88f49
Wellington Max Cruz <wellington.max1@gmail.com>
2020-10-05 22:43:57 -0300

    Corrigindo BUG stackOverFlow. Recursividade entre classes de contatos e pessoa.

--- src/main/java/com/example/algamoney/api/model/Pessoa.java
+++ src/main/java/com/example/algamoney/api/model/Pessoa.java
@@ -1,6 +1,7 @@
 package com.example.algamoney.api.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
 import javax.validation.Valid;
@@ -24,6 +25,7 @@ public class Pessoa {
 	@Embedded
 	private Endereco endereco;
 
+	@JsonIgnoreProperties("pessoa")
 	@Valid
 	@OneToMany(mappedBy = "pessoa", cascade = CascadeType.ALL)
 	private List<Contato> contatos;
