8f71fd463c8ab38937881808c7cfc56f50d5a723
Wellington Max Cruz <wellington.max1@gmail.com>
2020-10-05 23:28:49 -0300

    Retirando atributos contatos no json de lancamentos.

--- src/main/java/com/example/algamoney/api/model/Lancamento.java
+++ src/main/java/com/example/algamoney/api/model/Lancamento.java
@@ -1,21 +1,12 @@
 package com.example.algamoney.api.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDate;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "lancamento")
@@ -48,7 +39,8 @@ public class Lancamento {
 	@ManyToOne
 	@JoinColumn(name = "codigo_categoria")
 	private Categoria categoria;
-	
+
+	@JsonIgnoreProperties("contatos")
 	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "codigo_pessoa")
