93ed5a57e1751fe78cca4bcc409534adfd853abe
Wellington Max Cruz <wellington.max1@gmail.com>
2020-10-07 00:29:03 -0300

    Classe de configuração para os serviços do S3.

--- src/main/java/com/example/algamoney/api/config/S3Config.java
+++ src/main/java/com/example/algamoney/api/config/S3Config.java
@@ -3,6 +3,7 @@ package com.example.algamoney.api.config;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.BucketLifecycleConfiguration;
@@ -12,6 +13,7 @@ import com.amazonaws.services.s3.model.lifecycle.LifecycleFilter;
 import com.amazonaws.services.s3.model.lifecycle.LifecycleTagPredicate;
 import com.example.algamoney.api.config.property.AlgamoneyApiProperty;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -23,6 +25,7 @@ public class S3Config {
     @Autowired
     private AlgamoneyApiProperty property;
 
+    @Bean
     public AmazonS3 amazonS3() {
         // obtendo as credendiciais cadastradas no property
         AWSCredentials credentials = new BasicAWSCredentials(property.getS3().getAccessKeyId(), property.getS3().getSecretAccessKey());
@@ -30,6 +33,7 @@ public class S3Config {
         // criacao da instancia no s3
         AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(Regions.US_EAST_1)
                 .build();
 
         // regra para criacao do bucket automaticamente, e exclusão de arquivos temporarios
@@ -37,7 +41,7 @@ public class S3Config {
             amazonS3.createBucket(new CreateBucketRequest(property.getS3().getBucket()));
 
             BucketLifecycleConfiguration.Rule regraExpiracao = new BucketLifecycleConfiguration.Rule()
-                    .withId("Regra de expiração de arquivos temporários")
+                    .withId("Regra de expiracao de arquivos temporarios")
                     .withFilter(new LifecycleFilter(new LifecycleTagPredicate(new Tag("expirar", "true"))))
                     .withExpirationInDays(1)
                     .withStatus(BucketLifecycleConfiguration.ENABLED);
