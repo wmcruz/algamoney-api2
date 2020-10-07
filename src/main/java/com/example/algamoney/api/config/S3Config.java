7f5817006b6a7b7184e421d646e2082369f2cfa9
Wellington Max Cruz <wellington.max1@gmail.com>
2020-10-06 23:15:01 -0300

    Criação da classe de configuração para o s3.

---
+++ src/main/java/com/example/algamoney/api/config/S3Config.java
@@ -0,0 +1,52 @@
+package com.example.algamoney.api.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.BucketLifecycleConfiguration;
+import com.amazonaws.services.s3.model.CreateBucketRequest;
+import com.amazonaws.services.s3.model.Tag;
+import com.amazonaws.services.s3.model.lifecycle.LifecycleFilter;
+import com.amazonaws.services.s3.model.lifecycle.LifecycleTagPredicate;
+import com.example.algamoney.api.config.property.AlgamoneyApiProperty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Classe para configurar o Amazon AWS S3
+ * @author Wellington
+ */
+@Configuration
+public class S3Config {
+    @Autowired
+    private AlgamoneyApiProperty property;
+
+    public AmazonS3 amazonS3() {
+        // obtendo as credendiciais cadastradas no property
+        AWSCredentials credentials = new BasicAWSCredentials(property.getS3().getAccessKeyId(), property.getS3().getSecretAccessKey());
+
+        // criacao da instancia no s3
+        AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+
+        // regra para criacao do bucket automaticamente, e exclusão de arquivos temporarios
+        if (!amazonS3.doesBucketExistV2(property.getS3().getBucket())) {
+            amazonS3.createBucket(new CreateBucketRequest(property.getS3().getBucket()));
+
+            BucketLifecycleConfiguration.Rule regraExpiracao = new BucketLifecycleConfiguration.Rule()
+                    .withId("Regra de expiração de arquivos temporários")
+                    .withFilter(new LifecycleFilter(new LifecycleTagPredicate(new Tag("expirar", "true"))))
+                    .withExpirationInDays(1)
+                    .withStatus(BucketLifecycleConfiguration.ENABLED);
+
+            BucketLifecycleConfiguration configuration = new BucketLifecycleConfiguration().withRules(regraExpiracao);
+
+            amazonS3.setBucketLifecycleConfiguration(property.getS3().getBucket(), configuration);
+        }
+
+        return amazonS3;
+    }
+}
