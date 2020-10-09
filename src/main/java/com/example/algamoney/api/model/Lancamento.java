ee61b8a4bd676907243e70e6b2f25fb976353394
Wellington Max Cruz <wellington.max1@gmail.com>
2020-10-08 23:07:49 -0300

    Adicionando colunas de anexo e url anexo.

--- src/main/java/com/example/algamoney/api/model/Lancamento.java
+++ src/main/java/com/example/algamoney/api/model/Lancamento.java
@@ -46,6 +46,11 @@ public class Lancamento {
 	@JoinColumn(name = "codigo_pessoa")
 	private Pessoa pessoa;
 
+	private String anexo;
+
+	@Transient // anotacao que nao persiste no BD
+	private String urlAnexo;
+
 	@JsonIgnore
 	public boolean isReceita() {
 		return TipoLancamento.RECEITA.equals(this.tipo);
@@ -122,6 +127,22 @@ public class Lancamento {
 
 	public void setPessoa(Pessoa pessoa) {
 		this.pessoa = pessoa;
+	}
+
+	public String getAnexo() {
+		return anexo;
+	}
+
+	public void setAnexo(String anexo) {
+		this.anexo = anexo;
+	}
+
+	public String getUrlAnexo() {
+		return urlAnexo;
+	}
+
+	public void setUrlAnexo(String urlAnexo) {
+		this.urlAnexo = urlAnexo;
 	}
 
 	// Hashcode and equals
