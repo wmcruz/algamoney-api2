7a3d5f74a2ff22025caa669da25ebec2f9e64cd0
Wellington Max Cruz <wellington.max1@gmail.com>
2020-10-10 13:22:38 -0300

    Adicionando metodos para remover e substituir arquivos no s3.

--- src/main/java/com/example/algamoney/api/storage/S3.java
+++ src/main/java/com/example/algamoney/api/storage/S3.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -74,6 +75,20 @@ public class S3 {
                 property.getS3().getBucket(), objeto, new ObjectTagging(Collections.emptyList()));
 
         this.amazonS3.setObjectTagging(setObjectTaggingRequest);
+    }
+
+    public void remover(String objeto) {
+        DeleteObjectRequest deleteObjectRequest = new DeleteObjectRequest(property.getS3().getBucket(), objeto);
+
+        this.amazonS3.deleteObject(deleteObjectRequest);
+    }
+
+    public void substituir(String objetoAntigo, String objetoNovo) {
+        if(StringUtils.hasText(objetoAntigo)) {
+            this.remover(objetoAntigo);
+        }
+
+        this.salvar(objetoNovo);
     }
 
     private String gerarNomeUnico(String originalFilename) {
